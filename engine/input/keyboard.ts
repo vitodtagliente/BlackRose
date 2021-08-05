590fd02ed859a4e891fefcb64dce44dee26fd659
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2021-08-05 13:21:29 +0200

    Update keyboard.ts

--- engine/input/keyboard.ts
+++ engine/input/keyboard.ts
@@ -2,21 +2,26 @@ import { Canvas } from "../application";
 import Device from "./device";
 import KeyCode from "./keycode";
 
+type KeyEvent = (key: KeyCode) => void;
+
 export default class Keyboard extends Device
 {
-    private _keyStates: Map<KeyCode, boolean>;
+    private _keys: Set<KeyCode>;
+
+    public onKeyDown: KeyEvent = (key: KeyCode) => { };
+    public onKeyUp: KeyEvent = (key: KeyCode) => { };
 
     public constructor(canvas: Canvas)
     {
         super(canvas);
-        this._keyStates = new Map<KeyCode, boolean>();
-    } 
-    
+        this._keys = new Set<KeyCode>();
+    }
+
     public isKeyPressed(keyCode: KeyCode): boolean
     {
-        if (this._keyStates.has(keyCode))
+        if (this._keys.has(keyCode))
         {
-            this._keyStates.delete(keyCode);
+            this._keys.delete(keyCode);
             return true;
         }
         return false;
@@ -24,29 +29,36 @@ export default class Keyboard extends Device
 
     public isKeysDown(keyCode: KeyCode): boolean
     {
-        return this._keyStates.has(keyCode);
+        return this._keys.has(keyCode);
     }
 
     public release(keyCode: KeyCode): void 
     {
-        this._keyStates.delete(keyCode);
+        this._keys.delete(keyCode);
     }
 
     public plugin(): boolean
     {
         document.addEventListener('keydown', (e: KeyboardEvent) =>
         {
-            // this._handleKeyDown(e.code as KeyCode);
+            const key: KeyCode = e.code as KeyCode;
+            this._keys.add(key);
 
             e.stopPropagation();
             e.preventDefault();
+
+            this.onKeyDown(key);
         });
+
         document.addEventListener('keyup', (e: KeyboardEvent) =>
         {
-            // this._handleKeyUp(e.code as KeyCode);
+            const key: KeyCode = e.code as KeyCode;
+            this._keys.delete(key);
 
             e.stopPropagation();
             e.preventDefault();
+
+            this.onKeyUp(key);
         });
 
         return true;
