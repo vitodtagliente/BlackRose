84dc6803e9113f04f8ea3750bab80169c1b13f31
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2021-08-09 09:51:40 +0200

    changes

--- engine/graphics/index.ts
+++ engine/graphics/index.ts
@@ -2,11 +2,15 @@ import API from "./api"
 import ContextFactory from "./context_factory"
 import Context from "./context"
 import Color from "./color"
+import Material from "./material"
+import Texture from "./texture"
 
 export
 {
     API,
     Color,
     Context,
-    ContextFactory
+    ContextFactory,
+    Material,
+    Texture
 }
