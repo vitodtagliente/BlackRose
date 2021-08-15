20b5a77f69861196797053b1c0e827ed17fbd6d3
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2021-08-15 22:16:35 +0200

    changes

---
+++ engine/graphics/webgl/index.ts
@@ -0,0 +1,12 @@
+import IndexBuffer from "./index_buffer"
+import Shader from "./shader"
+import ShaderProgram from "./shader_program"
+import VertexBuffer from "./vertex_buffer"
+
+export
+{
+    IndexBuffer,
+    Shader,
+    ShaderProgram,
+    VertexBuffer
+}
