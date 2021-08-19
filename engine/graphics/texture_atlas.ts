a5092c751e6f77336d506ad5414891c576bd22e0
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2021-08-19 09:08:55 +0200

    changes

---
+++ engine/graphics/texture_atlas.ts
@@ -0,0 +1,16 @@
+import { Texture, TextureRect } from ".";
+
+export default class TextureAtlas
+{
+    private _texture: Texture;
+    private _sections: Map<string, TextureRect>;
+
+    public constructor(texture: Texture, sections?: Map<string, TextureRect>)
+    {
+        this._texture = texture;
+        this._sections = sections ? sections : new Map<string, TextureRect>();
+    }
+
+    public get texture(): Texture { return this._texture; }
+    public get sections(): Map<string, TextureRect> { return this._sections; }
+}
--- engine/graphics/texture_rect.ts
+++ engine/graphics/texture_rect.ts
@@ -2,12 +2,21 @@ import { TextureCoords } from ".";
 
 export default class TextureRect
 {
-    public origin: TextureCoords;
-    public size: TextureCoords;
+    private _origin: TextureCoords;
+    private _size: TextureCoords;
 
     public constructor(origin?: TextureCoords, size?: TextureCoords)
     {
-        this.origin = origin ? origin : TextureCoords.zero;
-        this.size = size ? size : TextureCoords.one;
+        this._origin = origin ? origin : TextureCoords.zero;
+        this._size = size ? size : TextureCoords.one;
+    }
+
+    public get origin(): TextureCoords { return this._origin; }
+    public get size(): TextureCoords { return this._size; }
+
+    public copy(r: TextureRect): void 
+    {
+        this._origin.copy(r._origin);
+        this._size.copy(r._size);
     }
 }
