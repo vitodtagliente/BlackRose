c5796a2b5a97506df502103109686c6c486ab441
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2021-09-19 16:21:11 +0200

    changes

--- game/src/tdk/projectile.ts
+++ game/src/tdk/projectile.ts
@@ -1,18 +1,45 @@
 import * as BlackRose from 'blackrose';
 import { Color, Renderer } from 'blackrose/graphics';
+import { Vector3 } from 'blackrose/math';
 import { World } from 'blackrose/scene';
 
 export default class Projectile extends BlackRose.Scene.Entity
 {
+    private _target: Vector3;
+    public speed: number;
+
     public constructor(name?: string)
     {
         super(name);
+        this.speed = 0.01;
+    }
+
+    public get hasTarget(): boolean { return this._target != null; }
+
+    public Follow(position: Vector3): void
+    {
+        this._target = position;
     }
 
     public update(world: World, deltaTime: number): void 
     {
         super.update(world, deltaTime);
 
+        if (this._target != null)
+        {
+            const direction: Vector3 = this._target.sub(this.transform.position).normalize();
+            this.transform.position.set(
+                this.transform.position.x + direction.x * this.speed * deltaTime,
+                this.transform.position.y + direction.y * this.speed * deltaTime,
+                this.transform.position.z
+            );
+
+            const distance: number = this.transform.position.distance(this._target);
+            if (distance <= 1)
+            {
+                world.destroy(this);
+            }
+        }
     }
 
     public render(renderer: Renderer): void 
@@ -20,8 +47,8 @@ export default class Projectile extends BlackRose.Scene.Entity
         super.render(renderer);
 
         renderer.gizmos.circle(
-            this.transform.position, 
-            Math.max(this.transform.scale.x, this.transform.scale.y), 
+            this.transform.position,
+            .2,
             Color.red()
         );
     }
--- game/src/tdk/tower.ts
+++ game/src/tdk/tower.ts
@@ -1,16 +1,21 @@
 import * as BlackRose from 'blackrose';
 import { Color, Renderer } from 'blackrose/graphics';
+import { Quaternion, Vector3 } from 'blackrose/math';
 import { World } from 'blackrose/scene';
+import { Projectile } from '.';
 import TargetFinder from './target_finder';
+import Timer from './timer';
 
 export default class Tower extends BlackRose.Scene.Entity
 {
     private _finder: TargetFinder;
+    private _timer: Timer;
 
     public constructor(name?: string)
     {
         super(name);
         this._finder = new TargetFinder();
+        this._timer = new Timer(600);
     }
 
     public update(world: World, deltaTime: number): void 
@@ -18,6 +23,16 @@ export default class Tower extends BlackRose.Scene.Entity
         super.update(world, deltaTime);
 
         this._finder.check(world, this.transform.position);
+        this._timer.tick(deltaTime);
+
+        if (this._finder.hasTarget && this._timer.isExpired)
+        {
+            let position: Vector3 = Vector3.zero();
+            this.transform.position.copy(position);
+            const projectile: Projectile = world.spawn(new Projectile("projectile"), position, Quaternion.identity());
+            projectile.Follow(this._finder.target.transform.position);
+            this._timer.reset();
+        }
     }
 
     public render(renderer: Renderer): void 
@@ -28,7 +43,7 @@ export default class Tower extends BlackRose.Scene.Entity
         if (this._finder)
         {
             renderer.gizmos.circle(this.transform.position, this._finder.radius, Color.yellow());
-            
+
             if (this._finder.hasTarget)
             {
                 renderer.gizmos.line(this.transform.position, this._finder.target.transform.position, Color.magenta());
